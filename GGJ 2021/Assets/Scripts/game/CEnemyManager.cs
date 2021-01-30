88edb409c308773189a357f1da89787d255be565
Nicolas Purgat <nicolaspurgat@hotmail.com>
2021-01-30 15:59:18 -0300

    Se agregó el manager de enemigos y el scroller de enemigos (spawnea y mueve enemigos)

---
+++ GGJ 2021/Assets/Scripts/game/CEnemyManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CEnemyManager : MonoBehaviour
+{
+
+
+    public static CEnemyManager Inst
+    {
+        get
+        {
+            if (_inst == null)
+                return new GameObject("Enemy Manager").AddComponent<CEnemyManager>();
+            return _inst;
+        }
+    }
+
+    private static CEnemyManager _inst;
+
+    public GameObject _enemyAsset;
+    private List<CEnemy> _enemies = new List<CEnemy>();
+
+    private void Awake()
+    {
+        if (_inst != null && _inst != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _inst = this;
+
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void addEnemy(CEnemy newEnemy)
+    {
+        _enemies.Add(newEnemy);
+    }
+
+
+
+}
