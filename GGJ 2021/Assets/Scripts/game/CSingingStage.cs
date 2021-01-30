2411fa0a9c164b9aca17010283f3c8fb29de013a
fedemello <fedemg15@gmail.com>
2021-01-30 16:26:33 -0300

    control logic and current device

--- GGJ 2021/Assets/Scripts/game/CSingingStage.cs
+++ GGJ 2021/Assets/Scripts/game/CSingingStage.cs
@@ -20,6 +20,17 @@ public class CSingingStage : CStateBase
     // Min distance to detect mouse movement.
     public float _mouseMinDistance = 30;
 
+    public float _distanceFromCenterToMouseBoundaries = 0.35f;
+
+    private float mTopLine;
+    private float mBotLine;
+
+    public GameObject _linePrefab;
+
+    private GameObject mLineOne;
+    private GameObject mLineTwo;
+    
+
     // Last mouse position in the Y axis.
     private float mCurrentMousePos = 0;
     private float mLastMousePos = 0;
@@ -27,6 +38,8 @@ public class CSingingStage : CStateBase
     public override void init()
     {
         base.init();
+
+        createLines();
 
         setState(STATE_INTRO);
     }
@@ -75,24 +88,124 @@ public class CSingingStage : CStateBase
         // Get the current mouse position.
         mCurrentMousePos = Input.mousePosition.y;
 
-        Debug.Log("last pos: " + mLastMousePos + " currentPos: " + mCurrentMousePos);
-
-        // At the end, update last position with current.
-        mLastMousePos = mCurrentMousePos;
-
-        // Keyboard
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Mathf.Abs(mCurrentMousePos - mLastMousePos) >= _mouseMinDistance)
         {
-
+            //Highlight if not highlighted.
+            updateHighlight(DEVICE_MOUSE);
         }
 
-        // Joystick
 
+
+        //Debug.Log("last pos: " + mLastMousePos + " currentPos: " + mCurrentMousePos);
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            //Left button
+            updateHighlight(DEVICE_KEYBOARD);
+        }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            //Right button
+            updateHighlight(DEVICE_KEYBOARD);
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Pause?
+        }
+
+
+
+        // Joystick
         if (mPlayerOne != null)
         {
             // Get current analog direction. Do stuff.
             //mPlayerOne.Direction.;
-        }        
+
+            if (mPlayerOne.AnyButtonIsPressed)
+            {
+                // Highlight if not highlighted.
+                updateHighlight(DEVICE_JOYSTICK);
+            }
+
+            //if (Input.GetKeyDown(KeyCode.S))
+            //{
+            //    mPlayerOne.Vibrate(1);
+            //}
+            //else if (Input.GetKeyDown(KeyCode.A))
+            //{
+            //    mPlayerOne.Vibrate(0, 5);
+            //}
+            //else if (Input.GetKeyDown(KeyCode.Q))
+            //{
+            //    mPlayerOne.Vibrate(5, 0);
+            //}
+            //else if (Input.GetKeyDown(KeyCode.W))
+            //{
+            //    mPlayerOne.Vibrate(5, 0);
+            //}
+            //else if (Input.GetKeyDown(KeyCode.D))
+            //{
+            //    mPlayerOne.StopVibration();
+            //}
+        }
+    }
+
+    private void checkKeyboardInput()
+    {
+        // Keyboard (avoid mouse input)
+        if (!Input.GetKey(KeyCode.Mouse0)
+              && !Input.GetKey(KeyCode.Mouse1)
+              && !Input.GetKey(KeyCode.Mouse2)
+              && !Input.GetKey(KeyCode.Mouse3)
+              && !Input.GetKey(KeyCode.Mouse4)
+              && !Input.GetKey(KeyCode.Mouse5)
+              && !Input.GetKey(KeyCode.Mouse6))
+        {
+            if (Input.anyKeyDown)
+            {
+                updateHighlight(DEVICE_KEYBOARD);
+            }
+        }
+    }
+
+    private void updateHighlight(int aType)
+    {
+        if (mCurrentDevice != aType)
+        {
+            int previousHighlight = mCurrentDevice;
+            // Unhighlight previous spot.
+
+            // Save new device.
+            mCurrentDevice = aType;
+
+            //Highlight new device.
+            Debug.Log("new device: " + mCurrentDevice);
+
+
+            if (previousHighlight == DEVICE_MOUSE)
+            {
+                // Update last position for detecting.
+                mLastMousePos = mCurrentMousePos;
+            }
+        }
+    }
+
+    public void createLines()
+    {
+        float x = Camera.main.pixelWidth / 2.0f;
+
+        float middle = Camera.main.pixelHeight / 2.0f;
+        float dist = middle * (_distanceFromCenterToMouseBoundaries / 2.0f);
+
+        Debug.Log("pixelMiddle: " + middle + " dist: " + dist);
+
+        mTopLine = middle - dist;
+        mBotLine = middle + dist;
+
+        mLineOne = Instantiate(_linePrefab, new Vector2(x, mTopLine), Quaternion.identity);
+        mLineTwo = Instantiate(_linePrefab, new Vector2(x, mBotLine), Quaternion.identity);
+
+        //mLineTwo = Instantiate(_linePrefab, );
     }
 
     public override void exitState()
